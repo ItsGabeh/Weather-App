a6997451ad84f911a77c0d89a9e92b270a39a873
ItsGabeh <itsbielrt@gmail.com>
2025-09-06 13:59:23 -0600

    Integrate components in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './components/header/header';
+import {Location} from './components/location/location';
+import {Content} from './components/content/content';
+import {Citymap} from './components/citymap/citymap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Location, Content, Citymap],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularEjemplo');
+  protected readonly title = signal('Weather App');
 }
